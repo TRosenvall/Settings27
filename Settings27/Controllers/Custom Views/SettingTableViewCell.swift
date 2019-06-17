a58fb4ec1951b461348440fa036fc591caaa5d3b
Timothy Rosenvall <timothy.rosenvall@gmail.com>
2019-06-17 10:53:52 -0600

    Setup Storyboard and File Organization

---
+++ Settings27/Controllers/Custom Views/SettingTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SettingTableViewCell.swift
+//  Settings27
+//
+//  Created by Timothy Rosenvall on 6/17/19.
+//  Copyright © 2019 Timothy Rosenvall. All rights reserved.
+//
+
+import UIKit
+
+class SettingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Settings27/Controllers/Model Controller/SettingController.swift
@@ -0,0 +1,9 @@
+//
+//  SettingController.swift
+//  Settings27
+//
+//  Created by Timothy Rosenvall on 6/17/19.
+//  Copyright © 2019 Timothy Rosenvall. All rights reserved.
+//
+
+import Foundation
